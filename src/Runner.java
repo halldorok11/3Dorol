ee465806f13903c1f684362a785befbd7ca545fe
OlafurD <olafur12@gmail.com>
2013-10-07 13:23:01 +0000

    Initial setup

---
+++ src/Runner.java
@@ -0,0 +1,13 @@
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+
+import java.util.List;
+
+
+public class Runner {
+
+	public static void main(String[] args) {
+
+	}
+
+}
